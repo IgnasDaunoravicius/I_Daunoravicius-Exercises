0ae55fe52ebc404f29b9960bcf4193161a4b123f
Ignas Daunoravicius <Ignas.Daunoravicius@stud.vtmc.lt>
2023-10-12 16:53:25 +0300

    pd4

---
+++ PD4/Js/main.js
@@ -0,0 +1,14 @@
+let petName;
+let petAge;
+
+petName = 'ziurke';
+petAge = '11';
+
+console.log('petName');
+console.log('petAge');
+
+petName = 'ziurkenas';
+petAge = 36;
+
+const buildingAdress = 'azuolo 13';
+console.log(buildingAdress);
